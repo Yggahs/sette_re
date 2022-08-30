9ad6a822d9dc061b94f144ae8a3521f33c684725
Alessio Calore <alessio.calore96@gmail.com>
2022-08-30 15:32:04 +0200

    updating

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { GiocoService } from './servizio-gioco.service';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -17,6 +17,7 @@ import { GiocoService } from './servizio-gioco.service';
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
